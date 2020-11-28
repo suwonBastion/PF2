14c9cb0e0e125ebd910f4b3538b4eb85faead33a
Bastion <msblastv2@gmail.com>
2020-11-28 16:24:18 +0900

    init

---
+++ src/main/java/com/bastion/bbu/BbuApplication.java
@@ -0,0 +1,13 @@
+package com.bastion.bbu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bastion/bbu/BbuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bastion.bbu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
